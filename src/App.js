f4e6c85c80b395598f5704bf6e78eb79d4542e07
Emrick19 <emricky17@gmail.com>
2021-05-08 22:49:45 +0100

    Add Modal Logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, {Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals"
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
 const App = () => {
+  const [ isCartShown, setIsCartShown ] = useState(false)
+
+  const showCartHandler = () => {
+    setIsCartShown(true)
+  }
+
+ const hideCartHandler = () => {
+  setIsCartShown(false)
+ }
   return (
     <Fragment>
-      <Header />
+    {isCartShown && <Cart onClose={hideCartHandler} />}      
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
