1cf8bc54baef93b0da10225ca936e170d6b7e580
Emrick19 <emricky17@gmail.com>
2021-05-08 22:51:07 +0100

    pass props to HeareCartButton component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,12 +3,12 @@ import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Foodie</h1>
-        <HeaderCartButton />
+        <HeaderCartButton click={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A food full of delicious food" />
