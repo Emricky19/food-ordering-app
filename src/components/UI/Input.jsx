4b82d72020569f3df016b5969ddd0358bd19b9a5
Emrick19 <emricky17@gmail.com>
2021-05-19 20:14:26 +0100

    Update Input component

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Input.module.css'
+import classes from "./Input.module.css";
 
-const Input = (props) => {
-    return (
-        <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-            <input {...props.input} />
-        </div>
-    )
-}
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.inputElement.id}>{props.label}</label>
+      <input ref={ref} {...props.inputElement} />
+    </div>
+  );
+});
 
-export default Input
+export default Input;
