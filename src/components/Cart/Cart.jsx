87eb2f74aa86b4c2f3c0a99b2dc5b35e7c8715d2
Emrick19 <emricky17@gmail.com>
2021-05-20 23:52:51 +0100

    Import CartItem component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,8 @@ import React, { useContext } from "react";
 import CartContext from "../../store/cart-context";
 
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -12,13 +14,29 @@ const Cart = (props) => {
 
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li key={item.id}>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
+
   return (
     <Modal click={props.onClose}>
       {cartItems}
