ef2dda84dca59e0ad29a207ee03b3bdf26055445
Emrick19 <emricky17@gmail.com>
2021-05-21 17:53:47 +0100

    Animate the header button

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [ btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const cartCxt = useContext(CartContext);
 
   const numberofCartItems = cartCxt.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`
+
+  const { items } = cartCxt
+
+  useEffect(() => {
+    if(items.length === 0){
+      return
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() =>{
+      setBtnIsHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+  
+
   return (
-    <button className={classes.button} onClick={props.click}>
+    <button className={btnClasses} onClick={props.click}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
