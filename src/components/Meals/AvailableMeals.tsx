00aa4c7ed7a9e583858d156c16f3b9c770251f15
Emrick19 <emricky17@gmail.com>
2021-05-25 22:29:50 +0100

    Convert jsx file to tsx

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../UI/Card";
-import MealItem from "../Meals/MealItem/MealItem";
+import MealItem from "./MealItem/MealItem";
 
 import classes from "./AvailableMeals.module.css";
 
