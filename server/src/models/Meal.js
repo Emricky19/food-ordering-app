4b23abf0fb8f9ab05a2e0a89be53e2e476a3741e
Emrick19 <emricky17@gmail.com>
2021-06-03 10:42:48 +0100

    Update Meal model

--- server/src/models/Meal.js
+++ server/src/models/Meal.js
@@ -1,10 +1,23 @@
-import { Schema, model } from "mongoose";
+import mongoose  from "mongoose";
 
-const mealSchema = new Schema({
-  id: String,
-  name: String,
-  description: String,
-  price: Number,
+
+const mealSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    default: 0.0,
+  },
 });
 
-export const Meal = model('Meal', mealSchema);
+export const Meal = mongoose.model("Meal", mealSchema);
