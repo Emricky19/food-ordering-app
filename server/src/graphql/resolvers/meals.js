5f94bf4b1684efba662c13fa63efb3ab623033d8
Emrick19 <emricky17@gmail.com>
2021-06-03 10:42:38 +0100

    Remove meals variable

--- server/src/graphql/resolvers/meals.js
+++ server/src/graphql/resolvers/meals.js
@@ -1,34 +1,17 @@
-const meals = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { Meal } from "../../models/Meal.js";
+
 
 const mealResolvers = {
   Query: {
-    meals: () => meals,
+    meals: async () => {
+      try {
+        const meals = await Meal.find();
+        return meals;
+      } catch (error) {
+          throw new Error(error)
+      }
+    },
   },
 };
 
-export default mealResolvers
+export default mealResolvers;
