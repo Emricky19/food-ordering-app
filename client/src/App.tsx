e6f26090c0beb6f0884975e221a1454063f71e56
Emrick19 <emricky17@gmail.com>
2021-06-05 21:28:20 +0100

    Add Authentication form handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import AuthForm from "./components/Auth/AuthForm"
 
 import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [isCartShown, setIsCartShown] = useState(false);
+  const [ isAuthFormShown, setIsAuthFormShown ] = useState(false);
 
   const showCartHandler = () => {
     setIsCartShown(true);
@@ -15,10 +17,19 @@ const App = () => {
   const hideCartHandler = () => {
     setIsCartShown(false);
   };
+
+  const showAuthForm = () => {
+    setIsAuthFormShown(true);
+  }
+
+  const hideAuthForm = () => {
+    setIsAuthFormShown(false)
+  }
   return (
     <CartProvider>
+      {isAuthFormShown && <AuthForm onClose={hideAuthForm} />}
       {isCartShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
+      <Header onShowCart={showCartHandler} onShowAuthForm={showAuthForm} />
       <main>
         <Meals />
       </main>
