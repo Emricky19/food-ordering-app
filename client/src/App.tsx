db0c1879d3563fc8e82e8e827b8a307623a2cac8
Emrick19 <emricky17@gmail.com>
2021-06-07 22:17:11 +0100

    Import AuthProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-import AuthForm from "./components/Auth/AuthForm"
+import AuthForm from "./components/Auth/AuthForm";
 
+import AuthProvider from "./store/auth-context";
 import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [isCartShown, setIsCartShown] = useState(false);
-  const [ isAuthFormShown, setIsAuthFormShown ] = useState(false);
+  const [isAuthFormShown, setIsAuthFormShown] = useState(false);
 
   const showCartHandler = () => {
     setIsCartShown(true);
@@ -20,20 +21,22 @@ const App = () => {
 
   const showAuthForm = () => {
     setIsAuthFormShown(true);
-  }
+  };
 
   const hideAuthForm = () => {
-    setIsAuthFormShown(false)
-  }
+    setIsAuthFormShown(false);
+  };
   return (
-    <CartProvider>
-      {isAuthFormShown && <AuthForm onClose={hideAuthForm} />}
-      {isCartShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} onShowAuthForm={showAuthForm} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        {isAuthFormShown && <AuthForm onClose={hideAuthForm} />}
+        {isCartShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} onShowAuthForm={showAuthForm} />
+        <main>
+          <Meals />
+        </main>
+      </CartProvider>
+    </AuthProvider>
   );
 };
 
