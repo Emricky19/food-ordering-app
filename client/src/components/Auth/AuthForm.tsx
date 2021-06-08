68c9ffc25806267afe4d3195db906add83a3dcd3
Emrick19 <emricky17@gmail.com>
2021-06-08 19:37:54 +0100

    Add loginUser Mutation

--- client/src/components/Auth/AuthForm.tsx
+++ client/src/components/Auth/AuthForm.tsx
@@ -1,24 +1,61 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
 import Modal from "../UI/Modal";
 import classes from "./AuthForm.module.css";
 
-const AuthForm: React.FC<{onClose: () => void}> = (props) => {
-  const [isLogin, setIsLogin] = useState(true);
+const LOGIN_USER = gql`
+  mutation LoginUser($username: String!, $password: String!) {
+    loginUser(username: $username, password: $password) {
+      token
+      id
+      username
+    }
+  }
+`;
 
-  const usernameInputRef = useRef<HTMLInputElement>(null);
-  const passwordInputRef = useRef<HTMLInputElement>(null);
+interface userCredentials {
+  username: string;
+  password: string;
+}
+
+const AuthForm: React.FC<{ onClose: () => void }> = (props) => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [values, setValues] = useState<userCredentials>({
+    username: "",
+    password: "",
+  });
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(_, result) {
+      console.log(result);
+    },
+    onError(err) {
+      // setErrors(err.graphQLErrors[0].extensions.exception.errors);
+      console.log(err.graphQLErrors[0].extensions!.exceptions.errors);
+    },
+    variables: values,
+  });
+
+  const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+    const { id, value } = e.currentTarget;
+
+    setValues((prevState) => {
+      return {
+        ...prevState,
+        [id]: value,
+      };
+    });
+  };
 
   const switchAuthModeHandler = () => {
     setIsLogin(!isLogin);
   };
 
   const submitHandler = (e: React.FormEvent) => {
-    const enteredUsername = usernameInputRef.current!.value;
-    const eneterPassword = passwordInputRef.current!.value;
-    console.log({
-      enteredUsername,
-      eneterPassword,
-    });
+    e.preventDefault();
+
+    loginUser();
   };
 
   return (
@@ -28,14 +65,19 @@ const AuthForm: React.FC<{onClose: () => void}> = (props) => {
         <form onSubmit={submitHandler}>
           <div className={classes.control}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" ref={usernameInputRef} required />
+            <input
+              type="text"
+              id="username"
+              onChange={changeHandler}
+              required
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="password">Your Password</label>
             <input
               type="password"
               id="password"
-              ref={passwordInputRef}
+              onChange={changeHandler}
               required
             />
           </div>
