d3cc538bbca1d5d947cf4e8319370774650055de
Emrick19 <emricky17@gmail.com>
2021-06-12 21:42:42 +0100

    Update Header.tsx

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -3,19 +3,22 @@ import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
-const Header: React.FC<{onShowCart: () => void, onShowAuthForm: () => void }> = (props) => {
-  return (
-    <>
-      <header className={classes.header}>
-        <h1>Foodie</h1>        
-        <h1 onClick={props.onShowAuthForm}>Login</h1>
-        <HeaderCartButton click={props.onShowCart} />
-      </header>
-      <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="A food full of delicious food" />
-      </div>
-    </>
-  );
-};
+const Header: React.FC<{ onShowCart: () => void; onShowAuthForm: () => void }> =
+  (props) => {
+    return (
+      <>
+        <header className={classes.header}>
+          <h1>Foodie</h1>
+          <div className={classes["header-items"]}>
+            <HeaderCartButton click={props.onShowCart} />
+            <span className={classes["material-icons"]} onClick={props.onShowAuthForm}>login</span>
+          </div>
+        </header>
+        <div className={classes["main-image"]}>
+          <img src={mealsImage} alt="A food full of delicious food" />
+        </div>
+      </>
+    );
+  };
 
 export default Header;
