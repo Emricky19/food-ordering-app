3c6ece9fc0928c8e17b2c55a61ed907d2ba2c9eb
Emrick19 <emricky17@gmail.com>
2021-06-12 22:36:51 +0100

    use AuthContext

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
+import { AuthContext } from "../../store/auth-context";
+
 const Header: React.FC<{ onShowCart: () => void; onShowAuthForm: () => void }> =
   (props) => {
+    const { user: isLoggedIn } = useContext(AuthContext);
     return (
       <>
         <header className={classes.header}>
           <h1>Foodie</h1>
           <div className={classes["header-items"]}>
             <HeaderCartButton click={props.onShowCart} />
-            <span className={classes["material-icons"]} onClick={props.onShowAuthForm}>login</span>
+            {!isLoggedIn && (
+              <span
+                className={classes["material-icons"]}
+                onClick={props.onShowAuthForm}
+              >
+                login
+              </span>
+            )}
           </div>
         </header>
         <div className={classes["main-image"]}>
